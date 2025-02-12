d39c103124626f2ac07e798e22903d9efa9dfa92
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-02-12 07:56:21 +0100

    clean

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import { Roboto } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const roboto = Roboto({
   variable: "--font-roboto",
